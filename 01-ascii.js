247ca927dd7dd100df31c83e0e05a1e99fe53c61
CDYC20 <cristydyc.20@gmail.com>
2024-06-12 13:14:28 +0200

    3 funciones

--- 01-ascii.js
+++ 01-ascii.js
@@ -1,13 +1,27 @@
+/*
 function ascii() {
-    // numero = Math.floor(Math.random() * 15000);
-    var numero = 0;
-    while(numero <= 100){
+    var numero = 47;
+    while(numero <= 110){
         numero++
         let caracter = String.fromCharCode(numero);
         console.log(caracter); 
     }
-    
 }
 ascii();
+*/
 
+function ascii(numero) { 
+    let caracter = String.fromCharCode(numero);
+    console.log(caracter);   
+}
+ascii(66);
+ascii(67);
 
+/*
+function ascii() {
+    numero = Math.floor(Math.random() * 255);
+    let caracter = String.fromCharCode(numero);
+    console.log(caracter);   
+}
+ascii();
+*/
